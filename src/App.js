4cc5e2cb2eecec4fd034da76ed127273c510aeb2
Eldad Chidra <eldadchb@gmail.com>
2024-09-15 22:30:24 +0200

    add: button css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import "./Button.css";
+import Button from "./Button";
+
+let sum = 0;
 
 function App() {
-  return <div className="App">Here is the App</div>;
+  return (
+    <div className="App">
+      <div className="button">
+        <Button
+          label="0"
+          onClick={() => {
+            sum += 2;
+            console.log(sum);
+          }}
+        />
+      </div>
+      <div className="button">
+        <Button
+          label="1"
+          onClick={() => {
+            sum += 2;
+            console.log(sum);
+          }}
+        />
+      </div>
+      <div className="button">
+        <Button
+          label="2"
+          onClick={() => {
+            sum += 2;
+            console.log(sum);
+          }}
+        />
+      </div>
+      <div className="button">
+        <Button
+          label="3"
+          onClick={() => {
+            sum += 2;
+            console.log(sum);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
